8879c1ef618b3acbb7dc72296657913f928b873a
Abdallah Serag <abdoserag862@gmail.com>
2026-05-21 01:54:52 +0300

    create splash screen

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -1,4 +1,5 @@
 class AppImages {
   static const String primaryBackground = 'assets/images/primarybackground.png';
   static const String scan = 'assets/images/scan.png';
+  static const String splash = 'assets/images/splash.png';
 }
