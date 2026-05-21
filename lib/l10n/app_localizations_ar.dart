0e3d008fbbf818864b46a0f04c598c747cd0733e
Abdallah Serag <abdoserag862@gmail.com>
2026-05-21 18:25:45 +0300

    intialize localization english & arabic in app

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,19 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get helloWorld => 'مرحبا بالعالم!';
+
+  @override
+  String get flutterDemo => 'ديمو فلاتر';
+
+  @override
+  String get hello => 'مرحبا';
+}
