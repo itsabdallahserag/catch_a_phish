a77efdf2d1a9958e3269c535adf98f6148981ea7
Abdallah Serag <abdoserag862@gmail.com>
2026-05-23 18:03:22 +0300

    the all content of authintication_screen

--- lib/ui/auth/auth_screen.dart
+++ lib/ui/auth/auth_screen.dart
@@ -1,7 +1,8 @@
-import 'package:catch_a_phish/ui/auth/rusablewidget/container_login_or_signup.dart';
+import 'package:catch_a_phish/core/utils/app_images.dart';
+import 'package:catch_a_phish/ui/auth/rusable_widget/container_auth.dart';
 import 'package:flutter/material.dart';
 
-class AuthScreen extends StatefulWidget{
+class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
 
   @override
@@ -13,24 +14,33 @@ class _AuthScreenState extends State<AuthScreen> {
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
-    return Container(            // Background image for the entire screen
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/images/primarybackground.png'),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            SizedBox(height: height * 0.1),
-            Image.asset('assets/images/splash.png',fit: BoxFit.cover, width: width * 1, height: height * 0.2),
-            SizedBox(height: height * 0.02),
-            ContainerLoginOrSignUp()
-          ]
+    return  Scaffold(
+      body: Container(
+            width: double.infinity,
+            height: double.infinity, // Background image for the entire screen
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppImages.primaryBackground),
+                fit: BoxFit.fill
+                ,
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  SizedBox(height: height * 0.05),
+                  Image.asset(
+                    AppImages.splash,
+                    fit: BoxFit.cover,
+                    width: width * 1,
+                    height: height * 0.2,
+                  ),
+                  ContainerAuth(),
+                ],
+              ),
+            ),
           ),
-      )
-        );
+    );
   }
 }
