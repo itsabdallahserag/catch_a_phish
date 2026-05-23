965d221751a6d16fdd1697e1e8410801aef31980
Abdallah Serag <abdoserag862@gmail.com>
2026-05-23 18:06:36 +0300

    chore: remove unused files

--- lib/ui/auth/rusable_widget/container_auth_tab.dart
+++ lib/ui/auth/rusable_widget/container_auth_tab.dart
@@ -1,11 +1,11 @@
 import 'package:catch_a_phish/core/utils/app_colors.dart';
-import 'package:catch_a_phish/ui/auth/rusablewidget/auth_tab.dart';
+import 'package:catch_a_phish/ui/auth/rusable_widget/container_auth_widgets/tab_content/auth_tab.dart';
 import 'package:flutter/material.dart';
 
-class LoginOrRegisterTab extends StatelessWidget {
+class ContainerAuthTab extends StatelessWidget {
   final int selectedIndex;
   final Function(int) onTabChanged;
-  const LoginOrRegisterTab({super.key,required this.selectedIndex,required this.onTabChanged});
+  const ContainerAuthTab({super.key,required this.selectedIndex,required this.onTabChanged});
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +28,7 @@ class LoginOrRegisterTab extends StatelessWidget {
             },
           ),
           AuthTab(
-            title: 'Register',
+            title: 'Sign Up',
             isSelected: selectedIndex == 1,
             onTap: () {
               onTabChanged(1);
