2e477ea81eab0108f10dbbb8626e23f42294d220
Abdallah Serag <abdoserag862@gmail.com>
2026-05-23 18:00:18 +0300

    container with background transparentblack contain the content of auth

---
+++ lib/ui/auth/rusable_widget/container_auth.dart
@@ -0,0 +1,46 @@
+import 'package:catch_a_phish/core/utils/app_colors.dart';
+import 'package:catch_a_phish/ui/auth/rusable_widget/container_auth_tab.dart';
+import 'package:catch_a_phish/ui/auth/rusable_widget/container_auth_widgets/form_login_or_signup/login_form.dart';
+import 'package:catch_a_phish/ui/auth/rusable_widget/container_auth_widgets/form_login_or_signup/signup_form.dart';
+import 'package:flutter/material.dart';
+
+class ContainerAuth extends StatefulWidget {
+  const ContainerAuth({super.key});
+
+  @override
+  State<ContainerAuth> createState() => _ContainerAuthState();
+}
+
+class _ContainerAuthState extends State<ContainerAuth> {
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: AppColors.transparentBlack,
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Column(
+          children: [
+            ContainerAuthTab(
+              selectedIndex: selectedIndex,
+              onTabChanged: (index) {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: selectedIndex == 0
+                  ? const LoginForm()
+                  : const SignUpForm(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
