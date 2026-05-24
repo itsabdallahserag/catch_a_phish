7f83e53815abbb4cbf03a063bdcc5d8f215f3eb0
Abdallah Serag <abdoserag862@gmail.com>
2026-05-24 04:33:35 +0300

    create ontap in authActionButton

--- lib/ui/auth/custom_widgets/auth_action_button.dart
+++ lib/ui/auth/custom_widgets/auth_action_button.dart
@@ -3,30 +3,34 @@ import 'package:catch_a_phish/core/utils/app_styles.dart';
 import 'package:flutter/material.dart';
 
 class AuthActionButton extends StatelessWidget {
-  const AuthActionButton({super.key});
+  final VoidCallback? onTap;
+  const AuthActionButton({super.key,required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      padding: const EdgeInsets.symmetric(vertical: 13),
-      decoration: BoxDecoration(
-        gradient: const LinearGradient(
-          colors: [AppColors.neonBlue, AppColors.darkBlue],
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-        ),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child:Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text(
-            'ACCESS SECURE TERMINAL',
-            style: AppStyles.medium16Black,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(vertical: 13),
+        decoration: BoxDecoration(
+          gradient: const LinearGradient(
+            colors: [AppColors.neonBlue, AppColors.darkBlue],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
           ),
-          Icon(Icons.arrow_forward, color: AppColors.black),
-        ],
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child:Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              'ACCESS SECURE TERMINAL',
+              style: AppStyles.medium16Black,
+            ),
+            Icon(Icons.arrow_forward, color: AppColors.black),
+          ],
+        ),
       ),
     );
   }
