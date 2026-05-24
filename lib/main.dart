d53183ad7956a58dfa2196158c75f2dc474ccb71
Abdallah Serag <abdoserag862@gmail.com>
2026-05-24 03:53:29 +0300

    integrate firebase in the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:catch_a_phish/core/utils/app_routes.dart';
+import 'package:catch_a_phish/firebase_options.dart';
 import 'package:catch_a_phish/l10n/app_localizations.dart';
 import 'package:catch_a_phish/ui/auth/auth_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
