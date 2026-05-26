1697556bf6055c0a7f52f0342f269e1e27deb41f
Abdallah Serag <abdoserag862@gmail.com>
2026-05-26 22:04:54 +0300

    adding images and styles

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -6,4 +6,8 @@ class AppImages {
   static const String google = 'assets/images/google.png';
   static const String iconEmail = 'assets/images/iconemail.png';
   static const String iconPassword = 'assets/images/iconpassword.png';
+  static const String iconHome = 'assets/images/iconhome.png';
+  static const String iconHistory = 'assets/images/iconhistory.png';
+  static const String iconProfile = 'assets/images/iconprofile.png';
+  static const String iconSettings = 'assets/images/iconsettings.png';
 }
