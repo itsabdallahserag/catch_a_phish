63401d2561edd77d24fac4518bdc508e170675f1
Abdallah Serag <abdoserag862@gmail.com>
2026-06-02 03:27:23 +0300

    the homeTab ui

--- lib/ui/home/tabs/home_tab/home_tab.dart
+++ lib/ui/home/tabs/home_tab/home_tab.dart
@@ -1,4 +1,9 @@
-import 'package:catch_a_phish/Core/utils/app_styles.dart';
+import 'package:catch_a_phish/Core/utils/app_colors.dart';
+import 'package:catch_a_phish/Core/utils/app_images.dart';
+import 'package:catch_a_phish/Ui/home/tabs/home_tab/widgets/engaged.dart';
+import 'package:catch_a_phish/Ui/home/tabs/home_tab/widgets/message_scan_widgets/message_scan.dart';
+import 'package:catch_a_phish/Ui/home/tabs/home_tab/widgets/tactical_over_view.dart';
+import 'package:catch_a_phish/Ui/home/tabs/home_tab/widgets/url_scan_widgets/url_scan.dart';
 import 'package:flutter/material.dart';
 
 class HomeTab extends StatelessWidget {
@@ -6,13 +11,44 @@ class HomeTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Welcome to the Home Tab!',
-          style: AppStyles.semiBold16White,
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+          toolbarHeight: height * 0.08,
+          backgroundColor: AppColors.glassGrey,
+          elevation: 0,
+          title: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(child: Image.asset(AppImages.splash,fit: BoxFit.cover, height: height * 0.14)),
+                SizedBox(width: width * 0.5), 
+                Expanded(
+                  child: CircleAvatar(
+                    backgroundColor: AppColors.white,
+                    child: Icon(Icons.person, color: Colors.grey),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-      ),
-    );
+      backgroundColor: AppColors.transparent,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TacticalOverView(),
+            SizedBox(height: height*0.02,),
+            Engaged(),
+            SizedBox(height: height*0.02,),
+            MessageScan(),
+            SizedBox(height: height*0.02,),
+            UrlScan()
+          ],
+            ),
+      )   );
   }
 }
