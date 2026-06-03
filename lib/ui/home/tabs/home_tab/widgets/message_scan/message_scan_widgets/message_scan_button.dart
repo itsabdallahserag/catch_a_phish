dd1d09390a4916a90699a4683c042e06405144b4
Abdallah Serag <abdoserag862@gmail.com>
2026-06-03 09:13:56 +0300

    button to navigate message scan screen

---
+++ lib/ui/home/tabs/home_tab/widgets/message_scan/message_scan_widgets/message_scan_button.dart
@@ -0,0 +1,64 @@
+import 'package:catch_a_phish/Core/utils/app_colors.dart';
+import 'package:catch_a_phish/Core/utils/app_images.dart';
+import 'package:catch_a_phish/Core/utils/app_styles.dart';
+import 'package:catch_a_phish/Ui/home/tabs/home_tab/widgets/message_scan/message_scan_screen.dart';
+import 'package:flutter/material.dart';
+
+class MessageScanButton extends StatelessWidget {
+  const MessageScanButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (context, animation, secondaryAnimation) =>
+                MessageScanScreen(),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+                  return FadeTransition(opacity: animation, child: child);
+                },
+          ),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(bottom: 16, left: 24, top: 16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [AppColors.black, AppColors.neonGreenOverlay69],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(16),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: AppColors.darkGrey,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Image.asset(AppImages.iconMessage),
+                ),
+                Spacer(),
+              ],
+            ),
+            Text('Message Scan', style: AppStyles.semiBold20White),
+            Text(
+              'Analyze SMS, email or technical text payloads for malicious strings.',
+              style: AppStyles.semiLight12LightGrey,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
