adea5abf56744462b87ed6b456ad11d442f08b29
Abdallah Serag <abdoserag862@gmail.com>
2026-06-03 09:14:33 +0300

    button to navigate url scan screen

---
+++ lib/ui/home/tabs/home_tab/widgets/url_scan/url_scan_widgets/url_scan_button.dart
@@ -0,0 +1,65 @@
+import 'package:catch_a_phish/Core/utils/app_colors.dart';
+import 'package:catch_a_phish/Core/utils/app_images.dart';
+import 'package:catch_a_phish/Core/utils/app_styles.dart';
+import 'package:catch_a_phish/Ui/home/tabs/home_tab/widgets/url_scan/url_scan_screen.dart';
+import 'package:flutter/material.dart';
+
+class UrlScanButton extends StatelessWidget {
+  const UrlScanButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (context, animation, secondaryAnimation) =>
+                UrlScanScreen(),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+                  return FadeTransition(opacity: animation, child: child);
+                },
+          ),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(
+          bottom: 16,left: 24,top: 16
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(colors: [
+            AppColors.blackOverlay80,
+            AppColors.deepBlue,
+          ],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          
+          children: [
+            Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(16),
+                
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: AppColors.darkGrey,
+                    borderRadius: BorderRadius.circular(8)
+                  ),
+                  child: Image.asset(AppImages.iconUrl)),
+                  Spacer()
+              ],
+            ),
+            Text('URL Scan',style: AppStyles.semiBold20White,),
+            Text('Check links before you click. Cross-references 50+ threat databases.',style: AppStyles.semiLight12LightGrey,),
+          ],
+        ),
+      ),
+    );
+  }
+}
