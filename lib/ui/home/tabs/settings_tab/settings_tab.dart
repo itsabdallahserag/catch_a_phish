91dd9d6f18d775e34e194c160aafd017ee4744ce
Abdallah Serag <abdoserag862@gmail.com>
2026-06-04 04:02:44 +0300

    add buttom logout

--- lib/ui/home/tabs/settings_tab/settings_tab.dart
+++ lib/ui/home/tabs/settings_tab/settings_tab.dart
@@ -1,3 +1,6 @@
+import 'package:catch_a_phish/Core/utils/app_colors.dart';
+import 'package:catch_a_phish/Ui/auth/auth_screen.dart';
+import 'package:catch_a_phish/Ui/auth/custom_widgets/auth_action_button.dart';
 import 'package:flutter/material.dart';
 
 class SettingsTab extends StatelessWidget {
@@ -7,7 +10,17 @@ class SettingsTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Settings Tab'),
+        child: AuthActionButton(
+          onTap: () {
+            Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (_) => const AuthScreen()),
+              (route) => false,
+            );
+          },
+          gradientColors: [AppColors.background, AppColors.coral],
+          child: Text('log out'),
+        ),
       ),
     );
   }
