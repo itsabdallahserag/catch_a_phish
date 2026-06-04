9273c64a78121d1a355a10d29061fb2d9b66e557
Abdallah Serag <abdoserag862@gmail.com>
2026-06-04 04:02:24 +0300

    change firebaseUtils methods to return usercredential not boolean

--- lib/Firbase_utils/firebase_utils.dart
+++ lib/Firbase_utils/firebase_utils.dart
@@ -3,68 +3,50 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 class FirebaseUtils {
   // Sign Up
-  static Future<bool> signUpWithEmailAndPassword(
+  static Future<UserCredential> signUpWithEmailAndPassword(
     String email,
     String password,
   ) async {
-    try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return true;
-    } on FirebaseAuthException catch (e) {
-      print(e.message);
-      return false;
-    }
+    return await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
   }
 
   // Login
-  static Future<bool> signInWithEmailAndPassword(
+  static Future<UserCredential> signInWithEmailAndPassword(
     String email,
     String password,
   ) async {
-    try {
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return true;
-    } on FirebaseAuthException catch (e) {
-      print(e.message);
-      return false;
-    }
+    return await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
   }
 
-  static Future<bool> loginByGoogle() async {
-    try {
-      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  // Google Sign In
+  static Future<UserCredential?> loginByGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-      if (googleUser == null) return false;
-
-      final GoogleSignInAuthentication googleAuth =
-          await googleUser.authentication;
-
-      final credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
-
-      await FirebaseAuth.instance.signInWithCredential(credential);
-      return true;
-    } catch (e) {
-      print(e.toString());
-      return false;
+    if (googleUser == null) {
+      return null;
     }
+
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
-  static Future<bool> loginByGitHub() async {
-  try {
+
+  // GitHub Sign In
+  static Future<UserCredential> loginByGitHub() async {
     final githubProvider = GithubAuthProvider();
-    await FirebaseAuth.instance.signInWithProvider(githubProvider);
-    return true;
-  } catch (e) {
-    print(e.toString());
-    return false;
+
+    return await FirebaseAuth.instance.signInWithProvider(githubProvider);
   }
-}
 }
