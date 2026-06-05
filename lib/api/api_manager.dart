930cfc669326d089e3403cb964649d25fb45b0cd
Abdallah Serag <abdoserag862@gmail.com>
2026-06-05 14:51:13 +0300

    file to handle api

---
+++ lib/api/api_manager.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:catch_a_phish/api/api_constants.dart';
+import 'package:catch_a_phish/api/end_points.dart';
+import 'package:catch_a_phish/api/models/PredictResponce.dart';
+import 'package:http/http.dart' as http;
+class ApiManager {
+  static Future<PredictResponce> spamCheck(String spamMessage) async {
+  try{
+    Uri url = Uri.https(
+    ApiConstants.spamBaseUrl,
+    EndPoints.spamApiName,
+  );
+
+  var response = await http.post(
+    url,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: jsonEncode({
+      'text': spamMessage,
+    }),
+  );
+  var responceBody = response.body ;
+  var json = jsonDecode(responceBody);
+  return PredictResponce.fromJson(json);
+  } catch (e) {
+    rethrow ;
+  }
+}
+}
