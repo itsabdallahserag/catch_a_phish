c6496b667680601c20501d39368cbf36dcc40c9f
Abdallah Serag <abdoserag862@gmail.com>
2026-06-08 05:18:22 +0300

    feat : modify of this files to be rusable

--- lib/ui/home/tabs/home_tab/widgets/url_scan/url_scan_widgets/ai_analysis.dart
+++ lib/ui/home/tabs/home_tab/widgets/url_scan/url_scan_widgets/ai_analysis.dart
@@ -4,42 +4,42 @@ import 'package:catch_a_phish/Core/utils/app_styles.dart';
 import 'package:flutter/material.dart';
 
 class AiAnalysis extends StatelessWidget {
-  const AiAnalysis({super.key});
+  final List<String> explanationList;
+  const AiAnalysis({super.key, required this.explanationList});
 
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
     return Container(
-                  padding: const EdgeInsets.all(12.0),
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [AppColors.blackOverlay80, AppColors.darkRed],
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                    ),
-                    borderRadius: BorderRadius.circular(24),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Image.asset(AppImages.iconAi, height: 26, width: 26),
-                          SizedBox(width: width * 0.04),
-                          Text(
-                            'AI Tactical Insight',
-                            style: AppStyles.semiBold20White,
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: height * 0.02),
-                      Text(
-                        '"This message contains high-frequency urgency triggers ("immediate", "permanent closure") and suspicious sender patterns. The embedded URL redirects to a non-standard TLD associated with credential harvesting."',
-                        style: AppStyles.light12White,
-                      ),
-                    ],
-                  ),
-                );
+      padding: const EdgeInsets.all(12.0),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [AppColors.blackOverlay80, AppColors.darkRed],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+        ),
+        borderRadius: BorderRadius.circular(24),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Image.asset(AppImages.iconAi, height: 26, width: 26),
+              SizedBox(width: width * 0.04),
+              Text('AI Tactical Insight', style: AppStyles.semiBold20White),
+            ],
+          ),
+          SizedBox(height: height * 0.02),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: explanationList.map((e) {
+              return Text("• $e", style: AppStyles.light12White);
+            }).toList(),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/ui/home/tabs/home_tab/widgets/url_scan/url_scan_widgets/confidence_and_ml.dart
@@ -0,0 +1,62 @@
+import 'package:catch_a_phish/Core/utils/app_colors.dart';
+import 'package:catch_a_phish/Core/utils/app_styles.dart';
+import 'package:flutter/widgets.dart';
+
+class ConfidenceAndMl extends StatelessWidget {
+  final num confidenceScore;
+  final num mlProbability;
+  const ConfidenceAndMl({
+    super.key,
+    required this.confidenceScore,
+    required this.mlProbability,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(26),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: AppColors.nightBackground,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text("CONFIDENCE SCORE", style: AppStyles.semiBold12LightGrey2),
+                Text(
+                  " ${confidenceScore.toDouble().toStringAsFixed(2)}%",
+                  style: AppStyles.semiBold12LightGrey2,
+                ),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(width: width * 0.01),
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(26),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: AppColors.nightBackground,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text("ML PROPABILITY", style: AppStyles.semiBold12LightGrey2),
+                Text(
+                  " ${mlProbability.toDouble().toStringAsFixed(2)}%",
+                  style: AppStyles.semiBold12LightGrey2,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
