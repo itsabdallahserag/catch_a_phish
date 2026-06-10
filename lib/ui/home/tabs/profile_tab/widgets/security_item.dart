a81520b396f3633143b4c069e46b132f49d4307a
Abdallah Serag <abdoserag862@gmail.com>
2026-06-10 10:17:22 +0300

    feat : creating profile tab by creating rusable widgets

---
+++ lib/ui/home/tabs/profile_tab/widgets/security_item.dart
@@ -0,0 +1,60 @@
+import 'package:catch_a_phish/Core/utils/app_colors.dart';
+import 'package:catch_a_phish/Core/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SecurityItem extends StatelessWidget {
+  final IconData icon;
+  final String titleText;
+  final String bodyText;
+  const SecurityItem({
+    super.key,
+    required this.icon,
+    required this.titleText,
+    required this.bodyText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return Container(
+      padding: EdgeInsets.all(14),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: AppColors.blackOverlay40,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: AppColors.glassGrey,
+            ),
+            child: Icon(icon,color: AppColors.white,size: 18,),
+          ),
+          SizedBox(width: width*0.04,),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(titleText, style: AppStyles.regular16White),
+              Text(bodyText, style: AppStyles.regular12LightGrey),
+            ],
+          ),
+          Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.arrow_forward_ios_rounded,
+              color: AppColors.mediumGrey,
+              size: 18,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
