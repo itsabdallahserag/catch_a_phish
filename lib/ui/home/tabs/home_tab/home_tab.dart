a2e2d0c29b8abe7103130b0220131064bbb89fff
Abdallah Serag <abdoserag862@gmail.com>
2026-06-12 16:33:34 +0300

    modify tactical view values

--- lib/ui/home/tabs/home_tab/home_tab.dart
+++ lib/ui/home/tabs/home_tab/home_tab.dart
@@ -1,13 +1,34 @@
 import 'package:catch_a_phish/Core/utils/app_colors.dart';
 import 'package:catch_a_phish/Core/utils/app_images.dart';
+import 'package:catch_a_phish/Firbase_utils/firebase_utils.dart';
+import 'package:catch_a_phish/Firbase_utils/models/user_model.dart';
 import 'package:catch_a_phish/Ui/home/tabs/home_tab/widgets/engaged.dart';
 import 'package:catch_a_phish/Ui/home/tabs/home_tab/widgets/message_scan/message_scan_widgets/message_scan_button.dart';
 import 'package:catch_a_phish/Ui/home/tabs/home_tab/widgets/tactical_over_view.dart';
 import 'package:catch_a_phish/Ui/home/tabs/home_tab/widgets/url_scan/url_scan_widgets/url_scan_button.dart';
 import 'package:flutter/material.dart';
 
-class HomeTab extends StatelessWidget {
+class HomeTab extends StatefulWidget {
   const HomeTab({super.key});
+
+  @override
+  State<HomeTab> createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  UserModel? user;
+
+  @override
+  void initState() {
+    super.initState();
+    getUser();
+  }
+
+  Future<void> getUser() async {
+    user = await FirebaseUtils.readUser();
+    if (!mounted) return;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,42 +36,52 @@ class HomeTab extends StatelessWidget {
     var width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-          toolbarHeight: height * 0.08,
-          backgroundColor: AppColors.glassGrey,
-          elevation: 0,
-          title: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(child: Image.asset(AppImages.splash,fit: BoxFit.cover, height: height * 0.14)),
-                SizedBox(width: width * 0.5), 
-                Expanded(
-                  child: CircleAvatar(
-                    backgroundColor: AppColors.white,
-                    child: Icon(Icons.person, color: Colors.grey),
-                  ),
+        toolbarHeight: height * 0.08,
+        backgroundColor: AppColors.glassGrey,
+        elevation: 0,
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Image.asset(
+                  AppImages.splash,
+                  fit: BoxFit.cover,
+                  height: height * 0.14,
                 ),
-              ],
-            ),
+              ),
+              SizedBox(width: width * 0.5),
+              Expanded(
+                child: CircleAvatar(
+                  backgroundColor: AppColors.white,
+                  child: Icon(Icons.person, color: Colors.grey),
+                ),
+              ),
+            ],
           ),
         ),
+      ),
       backgroundColor: AppColors.transparent,
       body: Padding(
-        padding:  EdgeInsets.all(12),
+        padding: EdgeInsets.all(12),
         child: SingleChildScrollView(
           child: Column(
             children: [
-              TacticalOverView(),
-              SizedBox(height: height*0.02,),
-              Engaged(),
-              SizedBox(height: height*0.02,),
-              MessageScanButton(),
-              SizedBox(height: height*0.02,),
-              UrlScanButton()
-            ],
+              TacticalOverView(
+                totalScans: user?.totalScans ?? 0,
+                threatsBlocked: user?.threatsBlocked ?? 0,
               ),
+              SizedBox(height: height * 0.02),
+              Engaged(),
+              SizedBox(height: height * 0.02),
+              MessageScanButton(),
+              SizedBox(height: height * 0.02),
+              UrlScanButton(),
+            ],
+          ),
         ),
-      )   );
+      ),
+    );
   }
 }
