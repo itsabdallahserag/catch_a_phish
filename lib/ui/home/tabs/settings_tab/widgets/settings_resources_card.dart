1d105a2a6e288c033f07ea81e3d2d516b6f813ed
Abdallah Serag <abdoserag862@gmail.com>
2026-06-17 13:59:28 +0300

    handle two widgets to use it

--- lib/ui/home/tabs/settings_tab/widgets/settings_resources_card.dart
+++ lib/ui/home/tabs/settings_tab/widgets/settings_resources_card.dart
@@ -1,9 +1,10 @@
 import 'package:catch_a_phish/Core/utils/app_colors.dart';
 import 'package:catch_a_phish/Core/utils/app_styles.dart';
+import 'package:catch_a_phish/Ui/home/tabs/home_tab/widgets/url_scan/webview/web_view_screen.dart';
 import 'package:catch_a_phish/Ui/home/tabs/settings_tab/widgets/settings_item.dart';
 import 'package:flutter/material.dart';
 
-class SettingsResourcesCard extends StatelessWidget{
+class SettingsResourcesCard extends StatelessWidget {
   const SettingsResourcesCard({super.key});
 
   @override
@@ -11,33 +12,49 @@ class SettingsResourcesCard extends StatelessWidget{
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
     return Container(
-          padding: EdgeInsets.all(14),
-          decoration: BoxDecoration(
-            color: AppColors.blackOverlay50,
-            borderRadius: BorderRadius.circular(14),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Resources', style: AppStyles.semiBold10LightGrey2),
-              SizedBox(height: height * 0.02),
-              SettingsItem(
-                isLanguage: true,
-                titleItem: 'Help Center',
-                description: '',
-                isActive: false,
-                onChanged: (value) {},
+      padding: EdgeInsets.all(14),
+      decoration: BoxDecoration(
+        color: AppColors.blackOverlay50,
+        borderRadius: BorderRadius.circular(14),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Resources', style: AppStyles.semiBold10LightGrey2),
+          SizedBox(height: height * 0.02),
+          SettingsItem(
+            onChanged: (value) {},
+            isLanguage: true,
+            titleItem: 'Help Center',
+            description: '',
+            isActive: true,
+            onPressedLanguage: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => WebViewScreen(
+                  url: 'https://catch-a-phish.web.app/help.html',
+                ),
               ),
-              SizedBox(height: height * 0.02),
-              SettingsItem(
-                isLanguage: true,
-                titleItem: 'Privacy Policy',
-                description: '',
-                isActive: true,
-                onChanged: (value) {},
-              ),
-            ],
+            ),
           ),
-        );
+          SizedBox(height: height * 0.02),
+          SettingsItem(
+            onChanged: (value) {},
+            isLanguage: true,
+            titleItem: 'Privacy Policy',
+            description: '',
+            isActive: true,
+            onPressedLanguage: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => WebViewScreen(
+                  url: 'https://catch-a-phish.web.app/privacy.html',
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
