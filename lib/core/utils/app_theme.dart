b3839f1efbbf83a75242f8e3460eb88f947ddc98
Abdallah Serag <abdoserag862@gmail.com>
2026-06-17 13:50:15 +0300

    creating app theme and locale providers to handle the theme and language

---
+++ lib/core/utils/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:catch_a_phish/Core/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: AppColors.white
+    )
+  );
+  static final ThemeData darkTheme = ThemeData(
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:catch_a_phish/Core/utils/app_routes.dart';
+import 'package:catch_a_phish/Core/utils/app_theme.dart';
+import 'package:catch_a_phish/Core/utils/notification_service.dart';
 import 'package:catch_a_phish/Ui/home/tabs/home_tab/widgets/message_scan/message_scan_screen.dart';
 import 'package:catch_a_phish/Ui/home/tabs/home_tab/widgets/url_scan/url_scan_screen.dart';
 import 'package:catch_a_phish/firebase_options.dart';
 import 'package:catch_a_phish/l10n/app_localizations.dart';
 import 'package:catch_a_phish/Ui/auth/auth_screen.dart';
 import 'package:catch_a_phish/Ui/home/home_screen.dart';
+import 'package:catch_a_phish/providers/app_locale_provider.dart';
+import 'package:catch_a_phish/providers/app_theme_provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +21,13 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await FirebaseFirestore.instance.enableNetwork();
-  runApp(const MyApp());
+  await NotificationService.init();
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (context) => AppThemeProvider(),),
+      ChangeNotifierProvider(create: (context) => AppLocaleProvider()),
+    ],
+    child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -25,6 +36,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    AppThemeProvider appThemeProvider = Provider.of<AppThemeProvider>(context);
+    AppLocaleProvider appLocaleProvider  = Provider.of<AppLocaleProvider>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
@@ -38,6 +51,10 @@ class MyApp extends StatelessWidget {
         AppRoutes.messageScan: (context) => MessageScanScreen(),
         AppRoutes.urlScan: (context) => UrlScanScreen(),
       },
+      themeMode: appThemeProvider.appTheme,
+      theme: AppTheme.lightTheme,
+      darkTheme:AppTheme.darkTheme ,
+      locale: Locale(appLocaleProvider.appLanguage),
     );
   }
 }
